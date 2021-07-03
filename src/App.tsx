46bb7f0b94d79421b458422a40167530f65b67f6
Maxsuel Fernando <maxsuel.dev@gmail.com>
2021-07-02 21:11:46 -0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import 'leaflet/dist/leaflet.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { FormEvent, useState } from 'react';
+import { Map, Marker, Popup, TileLayer } from "react-leaflet";
+import Leaflet from "leaflet";
+import { v4 as uuidv4 } from "uuid";
+
+import { fetchLocalMapBox } from "./apiMapBox";
+import AsyncSelect from "react-select/async";
+
+import mapPackage from "./package.svg";
+import mapPin from "./pin.svg";
+
+import "./App.css";
+
+const initialPosition = { lat: -22.2154042, lng: -54.8331331 };
+
+const mapPackageIcon = Leaflet.icon({
+  iconUrl: mapPackage,
+  iconSize: [58, 68],
+  iconAnchor: [29, 68],
+  popupAnchor: [170, 2],
+});
+
+const mapPinIcon = Leaflet.icon({
+  iconUrl: mapPin,
+  iconSize: [58, 68],
+  iconAnchor: [29, 68],
+  popupAnchor: [170, 2],
+});
+
+interface Delivery {
+  id: string;
+  name: string;
+  address: string;
+  complement: string;
+  latitude: number;
+  longitude: number;
 }
 
-export default App;
+type Position = {
+  longitude: number;
+  latitude: number;
+};
